9d3e170101ff5e6a29d43ab3f7c7b849b5dbfc38
Seba Viberti <sebaviberti2002@gmail.com>
2022-11-28 20:45:58 -0300

    Tercera entrega

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Creacion de productos
 class Zapatilla{        
     constructor(id, marca, nombre, precio, stock){
         this.id = id
@@ -8,104 +9,64 @@ class Zapatilla{
     }
 }
 
-const zapatilla1 = new Zapatilla(0, 'puma', 'incinerate', 1500, 15)
-const zapatilla2 = new Zapatilla(0, 'nike', 'airmax', 2000, 10)
-const zapatilla3 = new Zapatilla(0, 'nike', 'vapormax', 2500, 8)
-const zapatilla4 = new Zapatilla(0, 'adidas', 'supernova', 1300, 20)
-const zapatilla5 = new Zapatilla(0, 'adidas', 'coreracer', 1800, 20)
+const zapatilla1 = new Zapatilla(1, 'puma', 'incinerate', 1500, 15)
+const zapatilla2 = new Zapatilla(2, 'nike', 'airmax', 2000, 10)
+const zapatilla3 = new Zapatilla(3, 'nike', 'vapormax', 2500, 8)
+const zapatilla4 = new Zapatilla(4, 'adidas', 'supernova', 1300, 20)
+const zapatilla5 = new Zapatilla(5, 'adidas', 'coreracer', 1800, 20)
 
 const stockZapatilla = [zapatilla1, zapatilla2, zapatilla3, zapatilla4, zapatilla5]
-let marcaSeleccionada = []
-const carro = []
 
+const divCartaProducto = document.getElementById('cartaProducto')
 
-function filtro (){
-    let filtroMarca = parseInt(prompt('Ingrese la marca de zapatillas que busca \n 1- Nike \n 2- Adidas \n 3- Puma'))
-    let i
-    
-    switch(filtroMarca){
-        case 1:
-            marcaSeleccionada = stockZapatilla.filter(element => element.marca === 'nike')
+stockZapatilla.forEach(producto =>{
+    divCartaProducto.innerHTML += 
+    `
+    <div id="${producto.id}" class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${producto.marca} - ${producto.nombre}</h5>
+            <p class="card-text">$${producto.precio}</p>
+            <button id="${producto.id}" class="btn btn-primary">Agregar al carro</button>
+        </div>
+    </div>    
+    `
+})
 
-            for (i = 0; i < marcaSeleccionada.length; i++){
-                marcaSeleccionada[i].id = i+1
-                console.log(marcaSeleccionada[i])
-            }
+// Carrito de compras
+const carrito = JSON.parse(localStorage.getItem('carrito')) || []
+console.log(carrito)
+const botonAgregar = document.querySelectorAll('.btn-primary')
 
-            break;
-        case 2:
-            marcaSeleccionada = stockZapatilla.filter(element => element.marca === 'adidas')
+botonAgregar.forEach(boton =>{
+    boton.onclick = () =>{
+        const productoSeleccionado = stockZapatilla.find(producto =>producto.id === parseInt(boton.id))
 
-            for (i = 0; i < marcaSeleccionada.length; i++){
-                marcaSeleccionada[i].id = i+1
-                console.log(marcaSeleccionada[i])
-            }
+        const productoCarrito = {...productoSeleccionado, cantidad:1}
 
-            break;
-        case 3:
-            marcaSeleccionada = stockZapatilla.filter(element => element.marca === 'puma')
-
-            for (i = 0; i < marcaSeleccionada.length; i++){
-                marcaSeleccionada[i].id = i+1
-                console.log(marcaSeleccionada[i])
-            }
-
-            break;
-        default:
-            alert('Esa opcion no esta disponible')
-            break;
-    }
-}
-
-filtro()
-let catalogo = 'Tenemos en stock: \n'
-
-
-function agregarCarro (){
-
-    for(item of marcaSeleccionada){
-        catalogo += `\n ${item.id}- ${item.marca} ${item.nombre} por $${item.precio}`
-    }
-
-    catalogo += '\n\n Para finalizar la compra ingrese 99'
-
-    let respuesta = parseInt(prompt(catalogo))
-
-    while(isNaN(respuesta)){
-        alert(`Solo puede ingresar numeros`)
-        respuesta = parseInt(prompt(catalogo))
-    }
-
-    while(respuesta != 99){
-        switch(respuesta){
-            case 1:
-                carro.push(marcaSeleccionada[0])
-                alert(`Se agrego el carrito ${marcaSeleccionada[0].marca} ${marcaSeleccionada[0].nombre}`)
-                break;
-            case 2:
-                carro.push(marcaSeleccionada[1])
-                alert(`Se agrego el carrito ${marcaSeleccionada[1].marca} ${marcaSeleccionada[1].nombre}`)
-                break;
-            default:
-                alert('Esa opcion no esta disponible')
-                break;
+        const indexCarrito = carrito.findIndex(producto => producto.id === productoCarrito.id)
+        if (indexCarrito === -1){
+            carrito.push(productoCarrito)
+        }else{
+            carrito[indexCarrito].cantidad ++
         }
-        respuesta = parseInt(prompt(catalogo))
+
+        localStorage.setItem('carrito',JSON.stringify(carrito))
     }
-    alert('Se cerro tu pedido')
-    mostrarCarro()
-}
+}) 
 
-let zapatillaCarro = 'Vas a llevar'
-let precioCarro = 0
+const botonFinalizar = document.getElementById('botonFinalizar')
 
-function mostrarCarro(){
-    for(itemElegido of carro){
-        zapatillaCarro += `\n - ${itemElegido.marca} ${itemElegido.nombre}`
-        precioCarro += itemElegido.precio
+botonFinalizar.onclick = () =>{
+    const carritoFinal = carrito.map (producto => producto.precio * producto.cantidad)
+    let costo = 0
+    let total = 'Vas a llevar'
+    carritoFinal.forEach (valor => {
+        costo += valor
+    })
+
+    for(itemElegido of carrito){
+        total += `\n - ${itemElegido.marca} ${itemElegido.nombre}`
     }
 
-    alert(`${zapatillaCarro} \n El total es: $${precioCarro}`)
+    alert(`${total} \n El total es: $${costo}`)
 }
-
-agregarCarro()
